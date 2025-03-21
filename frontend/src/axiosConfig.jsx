780be518507848df1c3b98c0acb5c1e18ec38db4
shekharmenon1 <n11081236@qut.edu.au>
2025-03-21 12:59:56 +0000

    changed ip adress

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://localhost:5001:test', // local
   baseURL: 'http://3.27.168.243:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
